380635294f0d9042445f29caa15cd7b68119a80a
Amael Béghin <abeghin@gmail.com>
2017-11-27 18:45:11 +0100

    premiere version statique du dashboard

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -5,15 +5,19 @@ class JourneysController < ApplicationController
   end
 
   def create
-    @journey = Journey.create(journey_params)
-    @journey.save
-    redirect_to journeys_path
+    @journey = Journey.new(journey_params)
+    @journey.user_id = current_user.id
+    if @journey.save
+      redirect_to journeys_path
+    else
+      redirect_to treehouse_path
+    end
   end
 
   private
 
   def journey_params
-    params.require(:journey).permit(:checkin, :checkout, :guests_number, :is_confirmed, :user, :treehouse)
+    params.require(:journey).permit(:checkin, :checkout, :guests_number, :is_confirmed, :treehouse_id)
   end
 
 end
