5bfeda92b2a62c850c59d0e364412f507f52b57b
anthony hamou <anthonyhamou@yahoo.fr>
2017-11-27 18:45:26 +0100

    create page booking a treehouse

--- app/controllers/treehouses_controller.rb
+++ app/controllers/treehouses_controller.rb
@@ -1,8 +1,15 @@
 class TreehousesController < ApplicationController
   def index
     @treehouses = Treehouse.where("location = ? AND capacity >= ?", params[:location], params[:capacity])
+  end
+
+  def show
+    @treehouse = Treehouse.find(params[:id])
+    @journey = Journey.new
+  end
 
   def treehouses_params
     params.require(:location).permit(:capacity)
   end
+
 end
