ec7327f90a9b91897a8d6133ba6af82c72af76f2
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-27 14:35:21 +0100

    done creating model & controller Treehouse

---
+++ app/controllers/treehouses_controller.rb
@@ -0,0 +1,2 @@
+class TreehousesController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127132043) do
+ActiveRecord::Schema.define(version: 20171127133013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "treehouses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.integer "price_per_night"
+    t.integer "capacity"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_treehouses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +44,5 @@ ActiveRecord::Schema.define(version: 20171127132043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "treehouses", "users"
 end
