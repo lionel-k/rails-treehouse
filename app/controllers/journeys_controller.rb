581bc52ead7e5082cd69b791c4da3ee86276bc9b
Amael Béghin <abeghin@gmail.com>
2017-11-27 17:05:33 +0100

    pull master

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -4,5 +4,17 @@ class JourneysController < ApplicationController
     @journeys = Journey.all
   end
 
+  def create
+    @journey = Journey.create(journey_params)
+    @journey.save
+    redirect_to journeys_path
+  end
+
+  private
+
+  def journey_params
+    params.require(:journey).permit(:checkin, :checkout, :guests_number, :is_confirmed, :user, :treehouse)
+  end
 
 end
+
