e47b56401291629f17418512e04d71c653e71827
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-27 14:46:25 +0100

    created first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :treehouses, only: [:index, :show, :create, :new]
+  resources :journeys, only: [:index, :create]
   root to: 'pages#home'
 end
