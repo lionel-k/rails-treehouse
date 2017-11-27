fba0bc9d4a96b94fd27a7401de430301f8e54612
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-27 17:46:54 +0100

    added params in the form to be able to fetch data from the db

--- app/controllers/treehouses_controller.rb
+++ app/controllers/treehouses_controller.rb
@@ -1,8 +1,14 @@
 class TreehousesController < ApplicationController
+
   def index
+    raise
     @treehouses = Treehouse.where("location = ? AND capacity >= ?", params[:location], params[:capacity])
+  end
+
+
+  private
 
   def treehouses_params
-    params.require(:location).permit(:capacity)
+    params.require(:treehouses).permit(:capacity, :location, :checkin, :checkout)
   end
 end
