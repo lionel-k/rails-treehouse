460a06db127d8e33e08ee75298846d1b761daf02
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-29 11:24:34 +0100

    omiting choosing a range on date search

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,6 @@
-// import flatpickr from "flatpickr"
-// import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-
-// // flatpickr(".datepicker", {})
-
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr(".datepicker", {
   altInput: true
