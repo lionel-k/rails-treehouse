f5c0dd2d8e8b48c1b244b4371bd1cc8b8bef68ce
Amelie Petetin <amelie.petetin@gmail.com>
2017-11-30 13:23:46 +0100

    changed simple form in form tag, added autocomplete, managed the params to index page

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,16 +1,16 @@
-// function autocomplete() {
-//   document.addEventListener("DOMContentLoaded", function() {
-//     var treehouseLocation = document.getElementById('treehouse_location');
+function autocomplete() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var treehouseLocation = document.getElementById('treehouse_location');
 
-//     if (treehouseLocation) {
-//       var autocomplete = new google.maps.places.Autocomplete(treehouseLocation, { types: [ 'geocode' ] });
-//       google.maps.event.addDomListener(treehouseLocation, 'keydown', function(e) {
-//         if (e.key === "Enter") {
-//           e.preventDefault(); // Do not submit the form on Enter.
-//         }
-//       });
-//     }
-//   });
-// }
+    if (treehouseLocation) {
+      var autocomplete = new google.maps.places.Autocomplete(treehouseLocation, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(treehouseLocation, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
 
-// export { autocomplete };
+export { autocomplete };
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -15,5 +15,5 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   }
 }
 
-// import { autocomplete } from '../components/autocomplete';
-// autocomplete();
+import { autocomplete } from '../components/autocomplete';
+autocomplete();
