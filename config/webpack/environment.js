d0c69f2829454acaba1ca2860d43a101f52b5cbb
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2019-03-14 18:00:31 +0100

    Fix deploy Heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,11 +2,10 @@ const { environment } = require('@rails/webpacker')
 
 // Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
-environment.plugins.set('Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
   })
 )
-
 module.exports = environment
